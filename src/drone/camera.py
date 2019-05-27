61c5322523044da9efa7696ec570bc8a25b4bc9c
Bryan Lopez <bryan13138@gmail.com>
2019-05-27 10:30:02 -0500

    Clean up camera code

--- src/drone/camera.py
+++ src/drone/camera.py
@@ -2,54 +2,33 @@ import numpy as np
 import cv2
 
 
-def find_features(img):
+def goodFeatures(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     corners = cv2.goodFeaturesToTrack(gray, 7, 0.01, 10)
-
     if corners is not None:
         corners = np.int0(corners)
         for i in corners:
-            x,y = i.ravel()
+            x, y = i.ravel()
             cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
 
-def contours(img):
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Gaussian blur
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-
-    # Color thresholding
-    ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)
-
-    # Find the contours of the frame
-    contours, hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
-
-    return contours
-
 def boundingRect(img):
-    cnts = contours(img)
-    if len(cnts) <= 0:
+    contours = getContours(img)
+    if len(contours) <= 0:
         return
 
-    cnt = max(cnts, key=cv2.contourArea)
-    if cnt is None:
+    maxContour = max(contours, key=cv2.contourArea)
+    if maxContour is None:
         return
 
-    rect = cv2.minAreaRect(cnt)
+    rect = cv2.minAreaRect(maxContour)
     box = cv2.boxPoints(rect)
     box = np.int0(box)
     cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
 
-def hough_lines(img):
-    # Convert to grayscale
+def houghLines(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Find the edges in the image using canny detector
     edges = cv2.Canny(gray, 50, 200)
-
-    # Detect points that form a line
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=10, maxLineGap=250)
 
     # Draw lines on the image
@@ -58,54 +37,30 @@ def hough_lines(img):
             x1, y1, x2, y2 = line[0]
             cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
-def contours(img):
-    # img = (255 - img)
-
-    # Convert to grayscale
+def getContours(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Gaussian blur
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-
-    # Color thresholding
     ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)
+    contours, hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
+    return contours
 
-    # Find the contours of the frame
-    contours, hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_SIMPLE)
+def drawContours(img):
+    contours = getContours(img)
 
-    ret, thresh = cv2.threshold(gray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print('...................')
-    print(len(contours))
-    print('-------------------')
-
-    # Find the biggest contour (if detected)
     if len(contours) > 0:
-        # c = max(contours, key=cv2.contourArea)
-        # M = cv2.moments(c)
-        #
-        # cx = int(M['m10']/M['m00'])
-        # cy = int(M['m01']/M['m00'])
-        #
-        # cv2.line(img,(cx,0),(cx,720),(0, 0, 255),1)
-        # cv2.line(img,(0,cy),(1280,cy),(0, 0, 255),1)
-        cv2.drawContours(img, contours, 1, (0, 255, 0), 3)
+        # Find the largest contour and draw its centroid
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        cv2.line(img, (cx, 0), (cx, 720), (0, 0, 255), 1)
+        cv2.line(img, (0, cy), (1280, cy), (0, 0, 255), 1)
 
-    #     if cx >= 120:
-    #         print("Turn Left!")
-    #
-    #     if cx < 120 and cx > 50:
-    #         print("On Track!")
-    #
-    #     if cx <= 50:
-    #         print("Turn Right")
-    #
-    # else:
-    #     print("I don't see the line")
+        cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
 
-# video_capture = cv2.VideoCapture(0)
-videoCapture = cv2.VideoCapture('http://192.168.4.92:8080/video')
+# videoCapture = cv2.VideoCapture(0)
+videoCapture = cv2.VideoCapture('http://192.168.43.92:8080/video')
 if not videoCapture.isOpened():
     raise Exception('Couldn\'t establish video connection')
 
@@ -113,12 +68,13 @@ while(True):
     # Capture the frames
     ret, img = videoCapture.read()
     if img is None:
-        print('no frame')
+        print('No frame found')
         continue
 
-    # find_features(img)
+    # goodFeatures(img)
     # boundingRect(img)
-    contours(img)
+    # houghLines(img)
+    drawContours(img)
 
     cv2.imshow('frame', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
