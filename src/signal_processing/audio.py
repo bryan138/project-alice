867bf691b7c315ed3e8d368de9be723faffa5d6b
Bryan Lopez <bryan13138@gmail.com>
2019-05-26 20:49:52 -0500

    Clean code of dtmf identification project

--- src/signal_processing/audio.py
+++ src/signal_processing/audio.py
@@ -52,45 +52,23 @@ mapping = [c - 1 for c in args.channels]
 SAMPLING_RATE = 8000
 BUFFER_SIZE = 512
 BUFFER_DISPLAY_SIZE = BUFFER_SIZE
-FTT_CAP = 25
+FFT_CAP = 25
 
 if args.hifi:
     SAMPLING_RATE = 48000
     BUFFER_SIZE = 512
     BUFFER_DISPLAY_SIZE = BUFFER_SIZE
-    FTT_CAP = 125
+    FFT_CAP = 125
 
 if args.lofi:
     SAMPLING_RATE = 44100
     BUFFER_SIZE = 256
     BUFFER_DISPLAY_SIZE = BUFFER_SIZE // 4
-    FTT_CAP = 75
+    FFT_CAP = 75
     args.downsample = 10
 
-ID_THRESHOLD = 200
 DUDES = ['jackson', 'nicolas', 'theo', 'yweweler']
 DUDES = ['jackson']
-
-DTMF = [
-    [941, 1336], #0
-    [697, 1209], #1
-    [697, 1336], #2
-    [697, 1477], #3
-    [770, 1209], #4
-    [770, 1336], #5
-    [770, 1477], #6
-    [852, 1209], #7
-    [852, 1336], #8
-    [852, 1477], #9
-    [941, 1209], #*
-    [941, 1477], ##
-    [697, 1633], #A
-    [770, 1633], #B
-    [852, 1633], #C
-    [941, 1633], #D
-]
-
-DTMF_CODE = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '#', 'A', 'B', 'C', 'D' ]
 
 q = queue.Queue()
 samples = np.array([])
@@ -106,63 +84,19 @@ def process_sample_bufffer(samples):
     fft = scipy.fftpack.fft(samples)
     trimmedFFT = np.abs(fft[:BUFFER_SIZE // 2]);
 
-    # Get FFT peaks
-    frequencyStep = (args.samplerate / (2 * args.downsample)) / (BUFFER_SIZE / 2)
-    peakDistance = 256 / frequencyStep
-    peaks = find_peaks(trimmedFFT, distance=peakDistance)[0]
-    peaks = peaks[peaks > (256 / frequencyStep)]
-    peaks = sorted(peaks, key=lambda x: trimmedFFT[x], reverse=True)
-    peakA = min(peaks[0], peaks[1])
-    peakB = max(peaks[0], peaks[1])
-    peakAF = xf[peakA]
-    peakBF = xf[peakB]
-
     # Plot FFT
-    fftData = np.interp(trimmedFFT, [0.0, FTT_CAP], [0, 1])
+    fftData = np.interp(trimmedFFT, [0.0, FFT_CAP], [0, 1])
     lines[1].set_ydata(fftData)
-    lines[1].set_markevery([peakA, peakB])
 
-    # Update peak frequency labels
-    fftTextA.set_text('{}Hz'.format(int(peakAF)))
-    if fftData[peakA] > 0.95:
-        offset = (BUFFER_SIZE / (-80 if args.hifi else 250)) * frequencyStep
-        fftTextA.set_x(peakAF + offset)
-        fftTextA.set_y(0.95)
-        fftTextA.set_horizontalalignment('left')
-    else:
-        fftTextA.set_x(peakAF)
-        fftTextA.set_y(fftData[peakA] + 0.035)
-        fftTextA.set_horizontalalignment('center')
-
-    fftTextB.set_text('{}Hz'.format(int(peakBF)))
-    if fftData[peakB] > 0.95:
-        fftTextB.set_x(peakBF + ((BUFFER_SIZE / 250) * frequencyStep))
-        fftTextB.set_y(0.95)
-        fftTextB.set_horizontalalignment('left')
-    else:
-        fftTextB.set_x(peakBF)
-        fftTextB.set_y(fftData[peakB] + 0.035)
-        fftTextB.set_horizontalalignment('center')
-
-    # Identify DTMF
+    # Identify data
     identified = False
-    dtmfNumber = '--'
+    result = '--'
     confidence = float('Inf')
-    for i, tone in enumerate(DTMF):
-        difference = abs(peakAF - tone[0]) + abs(peakBF - tone[1])
 
-        if (difference < ID_THRESHOLD and difference < confidence):
-            identified = True
-            dtmfNumber = DTMF_CODE[i]
-            confidence = difference
-
-    dtmfText.set_text(dtmfNumber)
+    resultText.set_text(result)
     confidenceText.set_text('{0:.2f}'.format(confidence) if identified else '')
 
 def audio_callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    if status:
-        print(status, file=sys.stderr)
     # Fancy indexing with mapping creates a (necessary!) copy:
     q.put(indata[::args.downsample, mapping])
 
@@ -226,7 +160,7 @@ def get_fingerprint(path, plot = False):
         fingerprint = fingerprint + (trimmedFFT / len(segments))
 
     if plot:
-        fftData = np.interp(fingerprint, [0.0, FTT_CAP], [0, 1])
+        fftData = np.interp(fingerprint, [0.0, FFT_CAP], [0, 1])
         lines[1].set_ydata(fftData)
 
     fingerprint[0] = 0
@@ -241,7 +175,7 @@ def get_master_fingerprint(number, plot = False):
     master_fingerprint = master_fingerprint / (len(DUDES) * 50)
 
     if plot:
-        fftData = np.interp(master_fingerprint, [0.0, FTT_CAP], [0, 1])
+        fftData = np.interp(master_fingerprint, [0.0, FFT_CAP], [0, 1])
         lines[1].set_ydata(fftData)
 
     return master_fingerprint
@@ -294,10 +228,8 @@ fftAxes = figure.add_subplot(grid[2:, :])
 bufferAxes = figure.add_subplot(grid[1, :3])
 textAxes = figure.add_subplot(grid[1, 3])
 
-# FTT plot
-fttLines = fftAxes.plot(xf, np.zeros((BUFFER_SIZE // 2)))
-fftTextA = fftAxes.text(0, 0, '', size=7, ha='center', va='center')
-fftTextB = fftAxes.text(0, 0, '', size=7, ha='center', va='center')
+# FFT plot
+fftLines = fftAxes.plot(xf, np.zeros((BUFFER_SIZE // 2)))
 fftAxes.axis((0, args.samplerate / (2.0 * args.downsample), 0, 1))
 fftAxes.set_xticks(np.linspace(0, (args.samplerate / args.downsample) // 2, 6))
 fftAxes.tick_params(left=False, labelleft=False, labelsize='x-small')
@@ -320,7 +252,7 @@ bufferAxes.yaxis.grid(True)
 bufferAxes.tick_params(bottom=False, labelbottom=False, left=False, labelleft=False)
 
 # Text plot
-dtmfText = textAxes.text(0.5, 0.5, '5', size=36, ha='center', va='center', weight='bold')
+resultText = textAxes.text(0.5, 0.5, '5', size=36, ha='center', va='center', weight='bold')
 confidenceText = textAxes.text(0.5, 0.25, '18.94', size=8, ha='center', va='center')
 textAxes.set_xticks([])
 textAxes.set_yticks([])
@@ -329,7 +261,7 @@ textAxes.axis('off')
 figure.tight_layout(pad=0.5)
 figure.canvas.mpl_connect('key_press_event', key_press)
 
-lines = [samplingLines[0], fttLines[0], bufferLines[0]]
+lines = [samplingLines[0], fftLines[0], bufferLines[0]]
 
 # Generate master fingerprints for all numbers
 master_fingerprints = []
@@ -340,12 +272,13 @@ for i in range(10):
 for i in range(10):
     get_accuracy(i, master_fingerprints)
 
-# test = get_fingerprint('./recordings/2_jackson_1.wav', plot=True)
-# print(identify_sample(master_fingerprints, test))
+# Quick change short circuit
+if not True:
+    sys.exit(1)
 
-# animation = FuncAnimation(figure, update_plot, interval=args.interval)
-# stream = sd.InputStream(
-#     device=args.device, channels=max(args.channels),
-#     samplerate=args.samplerate, callback=audio_callback)
-# with stream:
-#     plt.show()
+animation = FuncAnimation(figure, update_plot, interval=args.interval)
+stream = sd.InputStream(
+    device=args.device, channels=max(args.channels),
+    samplerate=args.samplerate, callback=audio_callback)
+with stream:
+    plt.show()
