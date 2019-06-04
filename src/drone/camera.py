e55a7028fc77aa34f9fb51feadc16f9909116c74
Bryan Lopez <bryan13138@gmail.com>
2019-06-04 16:02:35 -0500

    Use adaptive thresholding

--- src/drone/camera.py
+++ src/drone/camera.py
@@ -67,7 +67,8 @@ def houghLines(img):
 def getContours(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    ret, thresh = cv2.threshold(blur, 128, 255, cv2.THRESH_BINARY_INV)
+    # ret, thresh = cv2.threshold(blur, 128, 255, cv2.THRESH_BINARY_INV)
+    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
